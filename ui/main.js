95fd0fb9506c42d36b84d0cef7d73536750b1ebe
keerthisan <30648142+keerthisan@users.noreply.github.com>
2017-08-09 11:08:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,2 @@
 console.log('Loaded!');
 
-var img=document.getElementById('modi');
-varmarginLeft-0;
-function moveRight(){
-    marginLeft = marginLeft+ 10;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick =function(){
-    var internal =setInterval(moveRight,100);
-
-    
-};
